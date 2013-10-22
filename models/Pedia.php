1538b8ee1fec22b4156d4b6cf9c0b1b421efa204
Matheus E. Muller <hello@memuller.com>
2013-10-22 02:53:17 -0200

    Updates labels and menu placement.

--- models/Pedia.php
+++ models/Pedia.php
@@ -9,25 +9,23 @@
 			'public' => true,'show_ui' => true,'show_in_menu' => true,'capability_type' => 'post', 'map_meta_cap' => true, 
 			'hierarchical' => false,'rewrite' => array('slug' => 'pedia'),'query_var' => true,
 			'supports' => array('custom-fields', 'title', 'thumbnail', 'editor'), 
-			'has_archive' => true, 'taxonomies' => array('post_tag', 'compendia')
+			'has_archive' => true, 'taxonomies' => array('post_tag')
 		) ;
 		static $labels = array (
 			'name' => 'Pedia',
 			'singular_name' => 'Article',
 			'menu_name' => 'Pedia',
 			'add_new' => 'Add new',
-			'add_new_item' => 'Add new spot',
+			'add_new_item' => 'Add new article',
 			'edit' => 'Update',
-			'edit_item' => 'Update spot',
-			'new_item' => 'Register spot',
+			'edit_item' => 'Update article',
+			'new_item' => 'Register article',
 			'view' => 'View',
-			'view_item' => 'View spot'
+			'view_item' => 'View article'
 		);
 		static $icon = '\f118' ;
-
-		static $fields = array(
-			
-		) ;
+		static $tabs ; 
+		static $fields = array() ;
 
 		static $formats = array('term', 'tool', 'module', 'project', 'reference', 'person');
 
