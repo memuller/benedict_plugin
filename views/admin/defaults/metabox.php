4e3e57ec0cecf04f2428b7e66d21d41fce7c2aa7
Matheus E. Muller <hello@memuller.com>
2013-10-22 02:52:09 -0200

    Improves display of descriptions on custom fields.

--- views/admin/defaults/metabox.php
+++ views/admin/defaults/metabox.php
@@ -9,14 +9,19 @@
 ?>
 <table class='form-table'>
 	<tbody>
-		<?php foreach ($fields as $field => $options) { 
-			require 'partials/field_handler.php'; ?>
+		<?php $i = 0; foreach ($fields as $field => $options) { 
+			require 'partials/field_handler.php'; 
+			$description_placing = ($options['type'] == 'text_area' || $options['type'] == 'geo') ? 'top' : 'bottom' ; ?>
+			<?php  
+				$header_style = $i == 0 ? 'padding-top: 5px; ' : '';
+				$header_style .= $description_placing != 'top' ? 'border-bottom: 0px; padding-bottom: 5px;' : '' ;
+			?>
 			<tr>
-				<th>
+				<th <?php echo "style='$header_style'" ?>>
 					<?php label($options['label'], $id  ) ?>
-					<?php if($options['type'] == 'text_area' || $options['type'] == 'geo'){ description($options['description']); } ?>
+					<?php if($description_placing == 'top'){ description($options['description']); } ?>
 				</th>
-				<td>
+				<td <?php echo "style='$header_style'" ?>>
 					<?php switch ($options['type']) {
 						
 						case 'boolean':
@@ -49,7 +54,14 @@
 
 					} ?>
 				</td>
-		<?php } ?>
+				<?php if ($description_placing != 'top'): ?>
+					</tr>
+					<tr class="compact"><td colspan='2' style="padding: 2px 5px 8px 5px ;">
+						<?php description($options['description']) ?>
+					</td>					
+				<?php endif ?>
+			</tr>
+		<?php $i ++ ; } ?>
 	</tbody>
 </table>
 
