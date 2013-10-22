cd408cd3da49dd34d3b7dceb3038fc9ad650268a
Matheus E. Muller <hello@memuller.com>
2013-10-22 02:51:23 -0200

    Migrates Sharedaddy settings.

--- base/Base.php
+++ base/Base.php
@@ -18,6 +18,27 @@
 
 		static $absent_roles = array();
 
+		static $migrations = array(
+			'0.1' => 'sharedaddy_options'
+		);
+
+		static function migrate_sharedaddy_options(){
+			update_option('jetpack_active_modules', array('vaultpress', 'sharedaddy', 'omnisearch'));
+			update_option( 'sharing-services', array( 
+				'visible' => array ( 'facebook', 'google-plus-1', 'twitter', 'pocket', 'pinterest', 'print' ), 
+				'hidden' => array()
+			));
+			update_option( 'sharing-options', array(
+				'global' => array(
+					'button_style' => 'icon', 
+					'sharing_label' => '',
+					'open_links' => 'same',
+					'show' => array('post', 'pedia'),
+					'custom' => array()
+				) 
+			));
+		}
+
 		static function build(){
 			parent::build();
 			add_filter( 'got_rewrite', '__return_true', 999 );
