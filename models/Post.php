dc598159147e0e68c52df681a310a50193c45561
Matheus E. Muller <hello@memuller.com>
2013-10-22 02:52:46 -0200

    Adds Post custom post class.

---
+++ models/Post.php
@@ -0,0 +1,29 @@
+<?php
+	namespace Benedict ;  
+	use CustomPost, DateTime, DateInterval ;
+
+	class Post extends CustomPost {
+
+		static $name = "post" ;
+		static $icon = '\f157' ;
+
+		static $fields = array( 
+			'featured' => array('label' => 'Destaque?', 'type' => 'boolean', 'default' => false,
+				'description' => 'no slider da página inicial.'),
+			'image_header' => array('label' => 'Usa imagem no cabeçalho?', 'type' => 'boolean', 'default' => false,
+				'description' => 'a mesma utilizada como thumbnail.')
+		) ;
+
+		static $editable_by = array(
+			'Apresentação' => array('fields' => array('featured', 'image_header'))
+		);
+
+		static $absent_actions = array('quick-edit');
+
+		static function build(){
+			$class = get_called_class();
+			parent::build();
+		}
+	}
+
+ ?>
