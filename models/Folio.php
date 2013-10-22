200ae75a220303eaa27ca319def57d1d520a15eb
Matheus E. Muller <hello@memuller.com>
2013-10-22 02:49:50 -0200

    Re-implements Folio as a custom post.

--- models/Folio.php
+++ models/Folio.php
@@ -1,40 +1,64 @@
 <?php
 	namespace Benedict ;  
-	use CustomTaxonomy, DateTime, DateInterval ;
+	use CustomPost, DateTime, DateInterval ;
 
-	class Folio extends CustomTaxonomy {
+	class Folio extends CustomPost {
 
-		static $name = "Folio" ;
-		static $applies_to = array('post', 'pedia');
-		static $settings = array( 
-			'hierarchical' => false,
-			'show_ui' => true,
-			'query_var' => true,
-			'rewrite' => array('slug' => 'folio'),
-			'show_admin_column' => true,
-			'update_count_callback' => '_update_post_term_count'
-		);
-		static $labels = array(
+		static $name = "folio" ;
+		static $creation_fields = array( 
+			'label' => 'folio','description' => 'A folio of items.',
+			'public' => true,'show_ui' => true,'show_in_menu' => true,'capability_type' => 'post', 'map_meta_cap' => true, 
+			'hierarchical' => false,'rewrite' => array('slug' => 'folio'),'query_var' => true,
+			'supports' => array('custom-fields', 'title', 'thumbnail', 'editor'), 
+			'has_archive' => true, 'taxonomies' => array(), 'menu_position' => 7
+		) ;
+		static $labels = array (
 			'name' => 'Folios',
 			'singular_name' => 'Folio',
-			'search_items' => 'Search Folios',
-			'popular_items' => 'Most frequent Folios',
-			'all_items' => 'All Folios',
-			'parent_item' => 'Parent Folio',
-			'parent_item_colon' => 'Parent Folio',
-			'edit_item' => 'Edit Folio',
-			'update_item' => 'Save Folio',
-			'add_new_item' => 'Add new Folio',
-			'new_item_name' => 'New Folio title'
+			'menu_name' => 'Folio',
+			'add_new' => 'Add new',
+			'add_new_item' => 'Add new folio',
+			'edit' => 'Update',
+			'edit_item' => 'Update folio',
+			'new_item' => 'Register folio',
+			'view' => 'View',
+			'view_item' => 'View folio'
 		);
+
+		static $icon = '\f322' ;
 
 		static $fields = array( 
 			
 		) ;
 
+		static $absent_actions = array('quick-edit');
+
 		static function build(){
 			$class = get_called_class();
 			parent::build();
+
+			add_action('p2p_init', function(){
+				p2p_register_connection_type(array(
+					'name' => 'items_to_folio',
+					'from' => array('pedia', 'post'),
+					'to' => 'folio',
+					'sortable' => 'any',
+					'title' => array(
+						'from' => 'Folio',
+						'to' => 'Conteúdo'
+					),
+					'admin_box' => array(
+						'show' => 'to',
+						'context' => 'side'
+					),
+					'from_labels' => array(
+						'singular_name' => 'Conteúdo',
+						'search_items' => 'Buscar Conteúdo',
+						'not_found' => 'Nada encontrado',
+						'create' => 'Incluir conteúdo'
+					)
+				));
+			});
 		}
 	}
 
