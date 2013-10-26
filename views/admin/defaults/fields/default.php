2d58b8bbc14a2b0ba1aa64d6390df5b6a6ade1bb
Matheus E. Muller <hello@memuller.com>
2013-10-26 14:27:05 -0200

    Updates Atlas; moving admin views to it.

--- views/admin/defaults/fields/default.php
+++
@@ -1,3 +0,0 @@
-<input type="text" <?php html_attributes( array( 
-	'name' => $name, 'id' => $id, 'value' => $object->$field, 'class' => 'text', 'size' => $size, 'style' => 'width: 100%;'
-	)) ?> <?php echo $html ?> <?php echo $validations ?> >
--- views/admin/defaults/partials/custom_single_field.php
+++
@@ -1,3 +0,0 @@
-<?php if(isset($custom_single)){?>
-	<input type="hidden" name="custom_single" value="<?php $custom_single = explode('\\', $custom_single); echo $custom_single[sizeof($custom_single)-1] ?>">
-<?php } ?>
