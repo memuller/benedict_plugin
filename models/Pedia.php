eca985c358ff967007856d5e73ddf4e6b6e72f43
Matheus E. Muller <hello@memuller.com>
2013-11-03 22:00:46 -0200

    Implements content-control fields/boxes on Pedia.

--- models/Pedia.php
+++ models/Pedia.php
@@ -26,13 +26,30 @@
 		);
 		static $icon = '\f118' ;
 		static $tabs ; 
-		static $fields = array(
-			'placeholder' => array()
+		static $fields = array( 
+			'featured' => array('label' => 'Destaque?', 'type' => 'boolean', 'default' => false,
+				'description' => 'no slider da página inicial.'),
+			'image_header' => array('label' => 'Usa imagem no cabeçalho?', 'type' => 'boolean', 'default' => false,
+				'description' => 'a mesma utilizada como thumbnail.'),
+			'claim' => array('label' => 'Chamada', 'type' => 'text', 'description' => 'usada no cabeçalho e slider de destaques.')
 		) ;
 
-		static $formats = array('term', 'tool', 'module', 'project', 'reference', 'person');
+		static $editable_by = array(
+			'Apresentação' => array('fields' => array('featured', 'image_header'), 'placing' => 'side'),
+			'Chamada' => array('fields' => array('claim'), 'placing' => 'normal')
+		);
 
 		static $absent_actions = array('quick-edit');
+
+		static $collumns = array(
+			'featured' => 'Destaque?'
+		);
+
+		public function featured(){
+			return $this->featured ? 'Sim' : 'Não' ;
+		}
+
+		static $formats = array('term', 'tool', 'module', 'project', 'reference', 'person');
 
 
 	}
